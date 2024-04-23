544df10a933d84bce2fc47b679d377d3ebdff717
unknown <asliddinabijonov04@gmail.com>
2024-04-23 11:22:38 +0500

    sadsd

--- AsliddinBlog/settings.py
+++ AsliddinBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ug_17o=%cg363=1z12v^k*cmjx#yv-0+^d@(3y9es(#=k5+ut%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-# STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
